8944f553489314ce3a865fc2070812a7b5b180e3
Victor3122 <129857663+Victor3122@users.noreply.github.com>
2023-06-05 17:01:00 +0630

    Update Royal-primeNo.php

--- Royal-primeNo.php
+++ Royal-primeNo.php
@@ -1,12 +1,12 @@
 <? function isPrime($number) {
-    // Check if the number is less than 2
+  
     if ($number < 2) {
         return false;
     }
 
-    // Loop from 2 to the square root of the number
+    
     for ($i = 2; $i <= sqrt($number); $i++) {
-        // Check if the number is divisible by any value in the loop
+      
         if ($number % $i === 0) {
             return false;
         }
